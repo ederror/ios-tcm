6ed1b03d23ae902c12551bf21a0d9879fe5f6fab
seonae <vkdtjsdo@gmail.com>
2021-06-18 02:13:15 +0900

    add PredictionResultViewController.swift

---
+++ CD_RECYCLE/CD_RECYCLE/IcePack+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  IcePack+CoreDataClass.swift
+//  CD_RECYCLE
+//
+//  Created by 백인찬 on 2021/06/16.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(IcePack)
+public class IcePack: NSManagedObject {
+
+}
